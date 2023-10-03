362ae4c4f169e3a8a39beb088cb8a6507b94d24a
PEPEBG6 <133446894+PEPEBG6@users.noreply.github.com>
2023-10-02 18:06:12 -0600

    Practica 3.1

---
+++ practica3.1/resources/views/components/layout.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <!-- <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title> -->
+</head>
+<body>
+    <x-alert type="danger" message="Soy un mensaje"/>
+    
+    <div class="content">
+        <div class="title m-b-md">
+            <x-alert></x-alert>
+        </div>    
+    </div>
+</body>
+</html>
